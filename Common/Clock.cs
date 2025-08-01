c2348b4e6f90df7a1c63994991e07a79d18fcf10
Anatolii Grynchuk <agrynco@gmail.com>
2025-08-01 21:44:18 +0300

    Add XML comments to IClock

--- Common/Clock.cs
+++ Common/Clock.cs
@@ -1,12 +1,44 @@
-﻿namespace Common;
+namespace Common;
 
+/// <summary>
+///     Provides access to the current date and time. This abstraction
+///     allows deterministic control over time in tests and other
+///     environments where predictable values are required.
+/// </summary>
 public interface IClock
 {
+    /// <summary>
+    ///     Gets the current local date and time.
+    /// </summary>
     DateTime Now { get; }
+
+    /// <summary>
+    ///     Gets today's date in the local time zone.
+    /// </summary>
     DateOnly Today { get; }
+
+    /// <summary>
+    ///     Returns the next occurrence of the specified day of week.
+    /// </summary>
+    /// <param name="dayOfWeek">The desired day of week.</param>
+    /// <returns>The upcoming date that falls on <paramref name="dayOfWeek"/>.</returns>
     DateOnly GetNearestDayOfWeek(DayOfWeek dayOfWeek);
+
+    /// <summary>
+    ///     Gets the last day of the current month.
+    /// </summary>
+    /// <returns>The final date in the month of <see cref="Today"/>.</returns>
     DateOnly GetTheLastDayInTheCurrentMonth();
+
+    /// <summary>
+    ///     Gets the first day of the next month.
+    /// </summary>
+    /// <returns>The date representing the beginning of the next month.</returns>
     DateOnly GetTheNextMonthBegin();
+
+    /// <summary>
+    ///     Gets the current time in Coordinated Universal Time (UTC).
+    /// </summary>
     DateTime UtcNow { get; }
 }
 
